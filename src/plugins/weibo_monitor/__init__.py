4c7e073c7020c6c317b1af99ed465f021f95f2cc
sugayoiya <no_moko@icloud.com>
2024-06-15 00:54:17 +0800

    :memo: update weibo doc

--- src/plugins/weibo_monitor/__init__.py
+++ src/plugins/weibo_monitor/__init__.py
@@ -14,7 +14,7 @@ from nonebot.plugin import PluginMetadata
 __plugin_meta__ = PluginMetadata(
     name='微博订阅',
     description='【微博订阅插件】\n'
-                '订阅并跟踪微博用户动态更新',
+                '订阅并跟踪微博用户动态更新, 电子宠物观察',
     usage='仅限私聊或群聊中群管理员使用:\n'
           '/微博订阅 [用户UID]\n'
           '/取消微博订阅 [用户UID]\n'
