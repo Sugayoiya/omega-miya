e56a5838c53e5afccca082084a7ab803298e5185
sugayoiya <no_moko@icloud.com>
2024-07-17 18:11:12 +0800

    :sparkles: add group context chat

--- src/plugins/nonebot_plugin_chatgpt_turbo/__init__.py
+++ src/plugins/nonebot_plugin_chatgpt_turbo/__init__.py
@@ -7,6 +7,8 @@ __plugin_meta__ = PluginMetadata(
     @bot发送问题时机器人不具有上下文回复的能力，tokens消耗较少，2s CD
     /chat 使用该命令进行问答时，bot具有上下文回复的能力，比较消耗tokens，暂定5s CD
     /clear 清除当前的群-用户上下文
+    /gchat 群接龙
+    /gclear 清楚群接龙上下文
     """,
     extra={'author': 'sugayoiya'},
 )
