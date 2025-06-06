363982df01870a192ffde704f2fdef5e8f18a248
Ailitonia <41713304+Ailitonia@users.noreply.github.com>
2025-06-06 22:58:25 +0800

    feat(plugin): 重构 `pixiv_pixivision_monitor` 插件使用统一订阅服务

--- src/plugins/pixiv_pixivision_monitor/command.py
+++ src/plugins/pixiv_pixivision_monitor/command.py
@@ -12,31 +12,23 @@ from typing import Annotated
 
 from nonebot.log import logger
 from nonebot.params import ArgStr, Depends
-from nonebot.plugin import CommandGroup
 
 from src.params.handler import get_command_str_single_arg_parser_handler
-from src.params.permission import IS_ADMIN
-from src.service import OmegaMatcherInterface as OmMI
-from src.service import OmegaMessageSegment, enable_processor_state
-from src.utils.pixiv_api import Pixivision
-from .helpers import (
-    add_pixivision_sub,
-    delete_pixivision_sub,
-    format_pixivision_article_message,
-    generate_pixivision_illustration_list_preview,
-)
-from .monitor import scheduler
+from src.service import OmegaMatcherInterface as OmMI, OmegaMessageSegment, OmegaSubscriptionHandlerManager
+from .subscription_source import PixivisionSubscriptionManager
 
-pixivision = CommandGroup(
-    'pixivision',
-    permission=IS_ADMIN,
-    priority=20,
-    block=True,
-    state=enable_processor_state(name='Pixivision', level=20),
+_pixivision_handler_manager = OmegaSubscriptionHandlerManager(
+    subscription_manager=PixivisionSubscriptionManager,
+    command_prefix='Pixivision',
+    aliases_command_prefix={'pixivision'},
+    default_sub_id='pixivision',
 )
 
+_pixivision = _pixivision_handler_manager.register_handlers()
+"""注册 Pixivision 订阅流程 Handlers"""
 
-@pixivision.command(
+
+@_pixivision.command(
     'query-articles-list',
     aliases={'Pixivision列表', 'pixivision列表'},
     permission=None,
@@ -54,14 +46,14 @@ async def handle_query_articles_list(
     await interface.send_reply('稍等, 正在获取 Pixivision 特辑列表~')
 
     try:
-        page_preview = await generate_pixivision_illustration_list_preview(page=int(page))
+        page_preview = await PixivisionSubscriptionManager.generate_pixivision_illustration_list_preview(int(page))
         await interface.send_reply(OmegaMessageSegment.image(url=page_preview.path))
     except Exception as e:
         logger.error(f'获取 Pixivision 特辑页面(page={page})失败, {e!r}')
         await interface.send_reply('获取 Pixivision 特辑列表失败, 可能是网络原因异常, 请稍后再试')
 
 
-@pixivision.command(
+@_pixivision.command(
     'query-article',
     aliases={'Pixivision特辑', 'pixivision特辑'},
     permission=None,
@@ -79,47 +71,11 @@ async def handle_query_article(
     await interface.send_reply('稍等, 正在获取 Pixivision 特辑作品内容~')
 
     try:
-        article_preview = await format_pixivision_article_message(article=Pixivision(aid=(int(aid))))
+        article_preview = await PixivisionSubscriptionManager.format_pixivision_article_message(int(aid))
         await interface.send_reply(article_preview)
     except Exception as e:
         logger.error(f'获取特辑(aid={aid})预览内容失败, {e!r}')
         await interface.send_reply('获取 Pixivision 特辑预览失败, 可能是网络原因异常, 请稍后再试')
-
-
-@pixivision.command(
-    'add-subscription',
-    aliases={'pixivision订阅', 'Pixivision订阅'},
-).handle()
-async def handle_add_subscription(interface: Annotated[OmMI, Depends(OmMI.depend())]) -> None:
-    scheduler.pause()  # 暂停计划任务避免中途检查更新
-    try:
-        await add_pixivision_sub(interface=interface)
-        await interface.entity.commit_session()
-        logger.success(f'{interface.entity}订阅 Pixivision 成功')
-        msg = '订阅 Pixivision 成功'
-    except Exception as e:
-        logger.error(f'{interface.entity}订阅 Pixivision 失败, {e!r}')
-        msg = '订阅 Pixivision 失败, 可能是网络异常或发生了意外的错误, 请稍后再试或联系管理员处理'
-    scheduler.resume()
-
-    await interface.finish_reply(msg)
-
-
-@pixivision.command(
-    'del-subscription',
-    aliases={'取消pixivision订阅', '取消Pixivision订阅'},
-).handle()
-async def handle_del_subscription(interface: Annotated[OmMI, Depends(OmMI.depend())]) -> None:
-    try:
-        await delete_pixivision_sub(interface=interface)
-        await interface.entity.commit_session()
-        logger.success(f'{interface.entity}取消订阅 Pixivision 成功')
-        msg = '已取消 Pixivision 订阅'
-    except Exception as e:
-        logger.error(f'{interface.entity}取消订阅 Pixivision 失败, {e!r}')
-        msg = '取消 Pixivision 订阅失败, 请稍后再试或联系管理员处理'
-
-    await interface.finish_reply(msg)
 
 
 __all__ = []
---
+++ src/plugins/pixiv_pixivision_monitor/subscription_source.py
@@ -0,0 +1,181 @@
+"""
+@Author         : Ailitonia
+@Date           : 2022/04/30 18:11
+@FileName       : subscription_source.py
+@Project        : nonebot2_miya
+@Description    : Pixivision 特辑作品图片预处理及插件工具
+@GitHub         : https://github.com/Ailitonia
+@Software       : PyCharm
+"""
+
+from typing import TYPE_CHECKING, Self
+
+from nonebot import logger
+
+from src.database import SocialMediaContentDAL, begin_db_session
+from src.database.internal.subscription_source import SubscriptionSource, SubscriptionSourceType
+from src.resource import TemporaryResource
+from src.service import OmegaMessage, OmegaMessageSegment
+from src.service.artwork_collection import PixivArtworkCollection
+from src.service.artwork_proxy import PixivArtworkProxy
+from src.service.omega_subscription_service import BaseSubscriptionManager
+from src.utils import semaphore_gather
+from src.utils.pixiv_api import Pixivision
+
+if TYPE_CHECKING:
+    from src.database.internal.social_media_content import SocialMediaContent
+    from src.utils.pixiv_api.model.pixivision import PixivisionArticle, PixivisionIllustration
+
+    type SMC_T = PixivisionIllustration
+
+
+class PixivisionSubscriptionManager(BaseSubscriptionManager['SMC_T']):
+    """Pixivision 订阅服务管理"""
+
+    def __init__(self) -> None:
+        self.sub_id = 'pixivision'
+
+    @classmethod
+    def init_from_sub_id(cls, sub_id: str | int = '') -> Self:
+        return cls()
+
+    @classmethod
+    def get_sub_type(cls) -> str:
+        return SubscriptionSourceType.pixivision.value
+
+    @classmethod
+    def _get_tmp_folder(cls) -> 'TemporaryResource':
+        """图片缓存文件夹"""
+        return TemporaryResource(cls.get_sub_type())
+
+    @staticmethod
+    def _get_smc_item_mid(smc_item: 'SMC_T') -> str:
+        return str(smc_item.aid)
+
+    @classmethod
+    def _parse_smc_item(cls, smc_item: 'SMC_T') -> 'SocialMediaContent':
+        raise NotImplementedError
+
+    async def _query_sub_source_smc_items(self) -> 'list[SMC_T]':
+        articles_data = await Pixivision.query_illustration_list()
+        return articles_data.illustrations[:8]
+
+    @staticmethod
+    async def _add_pixivision_article_artworks_into_database(article_data: 'PixivisionArticle') -> None:
+        """向数据库中写入 Pixivision 特辑文章中的作品"""
+        add_artwork_tasks = [
+            PixivArtworkCollection(x.artwork_id).add_artwork_into_database_ignore_exists()
+            for x in article_data.artwork_list
+        ]
+        await semaphore_gather(tasks=add_artwork_tasks, semaphore_num=8, return_exceptions=False)
+
+    @classmethod
+    async def _add_upgrade_smc_item(cls, smc_item: 'SMC_T') -> None:
+        article = Pixivision(aid=smc_item.aid)
+        article_data = await article.query_article()
+
+        # 先写入特辑文章中的作品信息
+        await cls._add_pixivision_article_artworks_into_database(article_data=article_data)
+
+        # 写入特辑文章信息
+        async with begin_db_session() as session:
+            await SocialMediaContentDAL(session=session).upsert(
+                source=cls.get_sub_type(),
+                m_id=str(article.aid),
+                m_type='pixivision_article',
+                m_uid='-1',
+                title=article_data.title_without_mark,
+                content=f'{article_data.description}\n{",".join(x.tag_name for x in article_data.tags_list)}',
+                ref_content=','.join(str(x.artwork_id) for x in article_data.artwork_list),
+            )
+
+    async def query_sub_source_data(self) -> 'SubscriptionSource':
+        return SubscriptionSource.model_validate({
+            'id': -1,
+            'sub_type': self.get_sub_type(),
+            'sub_id': self.sub_id,
+            'sub_user_name': 'pixivision',
+            'sub_info': 'Pixivision特辑订阅',
+        })
+
+    @classmethod
+    async def format_pixivision_article_message(cls, aid: int) -> str | OmegaMessage:
+        """处理 Pixivision 特辑更新为消息"""
+        article = Pixivision(aid=aid)
+        article_data = await article.query_article()
+        send_message = f'《{article_data.title}》\n'
+        try:
+            if article_data.eyecatch_image is None:
+                raise ValueError('article eyecatch image not found')
+
+            eyecatch_image = await article.download_resource(
+                url=article_data.eyecatch_image,
+                save_folder=cls._get_tmp_folder()('eyecatch'),
+                custom_file_name=f'eyecatch_{article.aid}.jpg'
+            )
+            send_message += OmegaMessageSegment.image(url=eyecatch_image.path)
+        except Exception as e:
+            logger.warning(f'PixivisionArticleMonitor | Query {article} eye-catch image failed, {e!r}')
+
+        send_message += f'\n{article_data.description}\n'
+
+        try:
+            title = f'Pixivision - {article_data.title_without_mark}'
+            preview_image = await cls._generate_pixivision_article_preview(title=title, article_data=article_data)
+            send_message += OmegaMessageSegment.image(url=preview_image.path)
+        except Exception as e:
+            logger.warning(f'PixivisionArticleMonitor | Query {article} preview image failed, {e!r}')
+
+        send_message += f'\n传送门: {article.url}'
+        return send_message
+
+    @classmethod
+    async def _format_smc_item_message(cls, smc_item: 'SMC_T') -> str | OmegaMessage:
+        return await cls.format_pixivision_article_message(aid=smc_item.aid)
+
+    @staticmethod
+    async def _generate_pixivision_article_preview(
+            title: str,
+            article_data: 'PixivisionArticle',
+    ) -> 'TemporaryResource':
+        """根据 Pixivision 特辑内容生成预览图"""
+        if article_data.illustration_list:
+            return await PixivArtworkProxy.generate_any_images_preview(
+                preview_name=title,
+                image_data=[
+                    (x.thumbnail, f'ArticleID: {x.aid}\n{x.split_title_without_mark}')
+                    for x in article_data.illustration_list
+                ],
+                preview_size=(480, 270),
+                hold_ratio=True,
+                num_of_line=4,
+            )
+        else:
+            return await PixivArtworkProxy.generate_artworks_preview(
+                preview_name=title,
+                artworks=[PixivArtworkProxy(x.artwork_id) for x in article_data.artwork_list],
+                preview_size=(512, 512),
+                num_of_line=4,
+            )
+
+    @staticmethod
+    async def generate_pixivision_illustration_list_preview(page: int = 1) -> 'TemporaryResource':
+        """根据 Pixivision Illustration 导览页面内容生成预览图"""
+        illustration_result = await Pixivision.query_illustration_list(page=page)
+        title = f'Pixivision Illustration - Page {page}'
+
+        return await PixivArtworkProxy.generate_any_images_preview(
+            preview_name=title,
+            image_data=[
+                (x.thumbnail, f'ArticleID: {x.aid}\n{x.split_title_without_mark}')
+                for x in illustration_result.illustrations
+            ],
+            preview_size=(480, 270),
+            hold_ratio=True,
+            num_of_line=4,
+        )
+
+
+__all__ = [
+    'PixivisionSubscriptionManager',
+]
