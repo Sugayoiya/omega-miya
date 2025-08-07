1ddb49a10aa73e1b5f426e96adff741369f27088
Ailitonia <ailitonia@gmail.com>
2025-08-07 22:57:27 +0800

    feat(plugin): Bot 消息撤回插件使用中间件统一撤回方法

--- src/plugins/bot_message_revoker/command.py
+++ src/plugins/bot_message_revoker/command.py
@@ -8,53 +8,38 @@
 @Software       : PyCharm
 """
 
-from nonebot.adapters.onebot.v11 import Bot as OneBotV11Bot
-from nonebot.adapters.onebot.v11 import MessageEvent as OneBotV11MessageEvent
-from nonebot.adapters.telegram import Bot as TelegramBot
-from nonebot.adapters.telegram.event import GroupMessageEvent as TelegramGroupMessageEvent
-from nonebot.adapters.telegram.event import PrivateMessageEvent as TelegramPrivateMessageEvent
+from typing import Annotated
+
 from nonebot.log import logger
+from nonebot.params import Depends
 from nonebot.permission import SUPERUSER
 from nonebot.plugin import on_command
 
+from src.service import OmegaMatcherInterface as OmMI
 from src.service import enable_processor_state
 
-self_recall = on_command(
+
+@on_command(
     'self-recall',
     aliases={'撤回'},
     permission=SUPERUSER,
     priority=10,
     block=True,
     state=enable_processor_state(name='SelfRecall', enable_processor=False),
-)
+).handle()
+async def handle_self_recall(
+    interface: Annotated[OmMI, Depends(OmMI.depend())],
+) -> None:
+    reply_msg_id = interface.get_event_reply_msg_id()
+    if not reply_msg_id:
+        return
 
-
-@self_recall.handle()
-async def handle_recall_onebot_v11(bot: OneBotV11Bot, event: OneBotV11MessageEvent):
-    if event.reply:
-        if bot.self_id == str(event.reply.sender.user_id):
-            message_id = event.reply.message_id
-            try:
-                await bot.delete_msg(message_id=message_id)
-                logger.success(f'SelfRecall | 撤回了{bot}消息(message_id={message_id!r})')
-            except Exception as e:
-                logger.error(f'SelfRecall | 撤回{bot}消息(message_id={message_id!r})失败, {e!r}')
-                await self_recall.finish('撤回消息部分或全部失败了', at_sender=True)
-
-
-@self_recall.handle()
-async def handle_recall_telegram(bot: TelegramBot, event: TelegramPrivateMessageEvent | TelegramGroupMessageEvent):
-    if event.reply_to_message:
-        if isinstance(event.reply_to_message, TelegramPrivateMessageEvent | TelegramGroupMessageEvent):
-            if bot.self_id == str(event.reply_to_message.from_.id):
-                chat_id = event.reply_to_message.chat.id
-                message_id = event.reply_to_message.message_id
-                try:
-                    await bot.delete_message(chat_id=chat_id, message_id=message_id)
-                    logger.success(f'SelfRecall | 撤回了{bot}消息(chat_id={chat_id!r}, message_id={message_id!r})')
-                except Exception as e:
-                    logger.error(f'SelfRecall | 撤回{bot}消息(chat_id={chat_id!r}, message_id={message_id!r})失败, {e!r}')
-                    await self_recall.finish('撤回消息部分或全部失败了')
+    try:
+        await interface.revoke_current_session_msg(message_id=reply_msg_id)
+        logger.success(f'SelfRecall | 撤回了{interface.bot}消息(message_id={reply_msg_id!r})')
+    except Exception as e:
+        logger.error(f'SelfRecall | 撤回{interface.bot}消息(message_id={reply_msg_id!r})失败, {e!r}')
+        await interface.finish_reply('撤回消息部分或全部失败了, 请检查是否有撤回权限')
 
 
 __all__ = []
